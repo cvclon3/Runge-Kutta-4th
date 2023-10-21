2c2a5d9cf5851456ae43e1e522a9ef7ed5950cef
Nikita Gharin <77896451+cvclon3@users.noreply.github.com>
2023-10-22 00:05:26 +0300

    add comments; remove unused files

--- main.cpp
+++ main.cpp
@@ -3,11 +3,34 @@
 #include "rk4.h"
 
 
+/*
+ * IMPORTANT
+ *
+ * If you want to use additional parameters in a function containing
+ * the right parts of the ode system, you need to create a structure
+ * of type "params" and initialize it BEFORE defining the ode function.
+ *
+ */
 struct params {
     float k = 0.0003;
 } params;
 
 
+/*
+ * IMPORTANT
+ *
+ * Any function on the right side of the odu must be defined using
+ * the following prototype:
+ *
+ * std::vector<float> %FUNC_NAME% (float t, std::vector<float> y, struct params *p)
+ *
+ * in which:
+ * :float t: - default argument, define as time, [s].
+ * :std::vector<float>: y - integration variables.
+ * :struct params *p: - structure with additional parameters for the
+ * function of the right parts of the odu system
+ *
+ */
 std::vector<float> func(
         float t,
         std::vector<float> y,
@@ -28,14 +51,29 @@ std::vector<float> func(
 
 int main() {
 
+    // Initialization of initial values
     std::vector<float> y0 {0, 100, 0, 100};
+
+    // Solving a given ODE system on a given time interval with
+    // given initial values and a given initial step
     std::vector<std::vector<float>> ans = rk4(&func, &params, 0, 5, &y0, 0.01);
 
+    // Output of the obtained values
     for (int i = 0; i < ans.size(); i++) {
         for (int j = 0; j < ans[i].size(); j++) {
             std::cout << ans[i][j] << " ";
         }
         std::cout << "\n";
+    }
+
+
+    // Solving a given system of oda at a certain moment with
+    // given values and a given step
+    std::vector<float> ans_iter = rk4_iteration(&func, &params, 0, &y0, 0.01);
+
+    // Output of the obtained values
+    for (auto & i : ans_iter) {
+        std::cout << i << " ";
     }
 
     return 0;
--- rk4.h
+++ rk4.h
@@ -22,7 +22,7 @@ std::vector<float> calc(
 
 std::vector<float> rk4_iteration(
         std::vector<float> (*func) (float, std::vector<float>, struct params*),
-        struct params* p,
+        struct params* param,
         float x,
         std::vector<float>* y,
         float h
@@ -33,10 +33,10 @@ std::vector<float> rk4_iteration(
 
     curr = {};
 
-    k1 = func(x, *y, p);
-    k2 = func(x + h/2, calc(y, &k1, h/2), p);
-    k3 = func(x + h/2, calc(y, &k2, h/2), p);
-    k4 = func(x + h, calc(y, &k3, h), p);
+    k1 = func(x, *y, param);
+    k2 = func(x + h/2, calc(y, &k1, h/2), param);
+    k3 = func(x + h/2, calc(y, &k2, h/2), param);
+    k4 = func(x + h, calc(y, &k3, h), param);
 
     for (int j = 0; j < len; j++) {
         curr.push_back(y->at(j) + h/6 * (k1[j] + 2*k2[j] + 2*k3[j] + k4[j]));
@@ -48,7 +48,7 @@ std::vector<float> rk4_iteration(
 
 std::vector<std::vector<float>> rk4(
         std::vector<float> (*func) (float, std::vector<float>, struct params*),
-        struct params* p,
+        struct params* param,
         float x0,
         float xf,
         std::vector<float>* y0,
@@ -65,10 +65,10 @@ std::vector<std::vector<float>> rk4(
     for (int i = 1; i < count; i++) {
         curr = {};
 
-        k1 = func(x, res.at(i - 1), p);
-        k2 = func(x + h/2, calc(&res.at(i - 1), &k1, h/2), p);
-        k3 = func(x + h/2, calc(&res.at(i - 1), &k2, h/2), p);
-        k4 = func(x + h, calc(&res.at(i - 1), &k3, h), p);
+        k1 = func(x, res.at(i - 1), param);
+        k2 = func(x + h/2, calc(&res.at(i - 1), &k1, h/2), param);
+        k3 = func(x + h/2, calc(&res.at(i - 1), &k2, h/2), param);
+        k4 = func(x + h, calc(&res.at(i - 1), &k3, h), param);
 
         for (int j = 0; j < len; j++) {
             curr.push_back(res[i-1][j] + h/6 * (k1[j] + 2*k2[j] + 2*k3[j] + k4[j]));
